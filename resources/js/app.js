d2eaeff47a821280739a81b0b67582f49d6434ee
WahyuSapurata <wm337708@gmail.com>
2025-07-27 00:06:43 +0800

    update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,4 @@
 import './bootstrap';
-import { registerSW } from 'virtual:pwa-register'
-
-console.log("📦 app.js loaded");
-
-registerSW({
-    immediate: true,
-    onRegistered(r) {
-        console.log('✅ Service Worker registered', r);
-    },
-    onRegisterError(error) {
-        console.error('❌ SW registration failed', error);
-    }
-});
-
 
 let deferredPrompt;
 
