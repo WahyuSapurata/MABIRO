ad0f637d7107ed9f20479f3ddc58a698a325db7d
WahyuSapurata <wm337708@gmail.com>
2025-07-27 00:57:30 +0800

    update vite

--- vite.config.js
+++ vite.config.js
@@ -52,7 +52,7 @@ export default defineConfig({
         manifest: true,
         outDir: 'public/build',
         rollupOptions: {
-            input: 'resources/js/app.js',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
         },
         emptyOutDir: true,
     }
