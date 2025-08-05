0be6f8af6cf17c3a2eed472494338803b1cab810
fauzanmsc <fauzanmsc@gmail.com>
2025-08-05 19:51:34 +0800

    Desc

--- resources/views/user/layouts/layout.blade.php
+++ resources/views/user/layouts/layout.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Solion - IT Solutions Template">
+    <meta name="description" content="Sistem Informasi Manajemen Asrama">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @php
